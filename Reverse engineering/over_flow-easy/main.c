92b6b73b22e0472d87112584d386b7e996892dbd
lvexol <aswinsree.sundaresh@gmail.com>
2024-09-05 07:48:48 -0400

    new101

--- Reverse engineering/over_flow-easy/main.c
+++ Reverse engineering/over_flow-easy/main.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Function to apply ROT13 encoding
+void rot13(char *str) {
+    while (*str) {
+        if ((*str >= 'a' && *str <= 'z')) {
+            *str = ((*str - 'a' + 13) % 26) + 'a';
+        } else if ((*str >= 'A' && *str <= 'Z')) {
+            *str = ((*str - 'A' + 13) % 26) + 'A';
+        }
+        str++;
+    }
+}
+
 // Thread-local storage for the flag
 __thread char tls_flag[] = "QUNOX0NURntDNFRDSC1USEUtTTAwTi01Njc4fQ==";
+
+// Function to encode the flag twice with ROT13
+void encode_tls_flag_twice() {
+    // Apply ROT13 encoding twice
+    rot13(tls_flag); // First ROT13
+    rot13(tls_flag); // Second ROT13
+}
 
 void math_trick_function(int a, int b) {
     int sum = a + b;
@@ -11,22 +30,28 @@ void math_trick_function(int a, int b) {
     printf("The sum of %d and %d is %d.\n", a, b, sum);
     printf("The product of %d and %d is %d.\n", a, b, product);
 }
+
 void red_herring_function() {
     printf("This is a red herring. Nothing important here.\n");
 }
+
 void give_flag() {
     puts("Congratulations! You've found the flag:");
     printf("%s\n", tls_flag);
 }
+
 void mislead_function() {
     printf("You're getting warmer, but this isn't the flag.\n");
 }
+
 void secret_function() {
     printf("You're really close now...\n");
 }
+
 void hidden_function() {
     printf("This is a hidden function. Keep exploring!\n");
 }
+
 void check_values(int x) {
     if (x == 42) {
         printf("You've found the magic number! But no flag here.\n");
@@ -34,12 +59,14 @@ void check_values(int x) {
         printf("The number %d is not the magic number. Keep trying.\n", x);
     }
 }
+
 void string_manipulation_function(char *input) {
     char result[100];
     strcpy(result, input);
     strcat(result, " - processed");
     printf("The manipulated string is: %s\n", result);
 }
+
 void arithmetic_challenge(int num) {
     int doubled = num * 2;
     int squared = num * num;
@@ -80,6 +107,7 @@ void check_palindrome(char *str) {
         printf("The string '%s' is not a palindrome.\n", str);
     }
 }
+
 void array_sum(int arr[], int size) {
     int sum = 0;
     for (int i = 0; i < size; i++) {
@@ -89,6 +117,9 @@ void array_sum(int arr[], int size) {
 }
 
 int main() {
+    // Encode the flag with ROT13 twice
+    encode_tls_flag_twice();
+    
     printf("Welcome to the CTF challenge!\n");
     vulnerable_function();
     printf("Program finished:)\n");
